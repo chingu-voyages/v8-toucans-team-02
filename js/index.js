dafc278f63b58ce03f41d2df2af0ebffd4f90ef4
hernandoagf <22449631+hernandoagf@users.noreply.github.com>
2019-04-21 16:21:08 -0300

    Añade seccion social con carrusel responsive

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/*var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {
+        slideIndex = 1;
+    }
+    x[2].style.display = "block";
+    setTimeout(carousel, 2000);
+}*/
