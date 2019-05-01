65059a843e13b914b97567b2ef562eda57195f01
hernandoagf <22449631+hernandoagf@users.noreply.github.com>
2019-05-01 18:28:10 -0300

    Modifica sección de testimonios con animación de tarjetas

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-/*var slideIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {
-        slideIndex = 1;
-    }
-    x[2].style.display = "block";
-    setTimeout(carousel, 2000);
-}*/
-
-
 /* Comportamiento del boton de Top */
 window.onscroll = function() {
 	scrollFunction();
