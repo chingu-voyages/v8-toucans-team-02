9678e411b6cc9c2c4c990d3184c50925122d84f8
FuZz8Sanchez <fuzzdigital@gmail.com>
2019-05-04 22:20:43 -0500

    Funciones JS cambio imagenes
    Termine de agregar las funciones JS para que los botones on click cambien imagenes de su respectivo container

--- js/index.js
+++ js/index.js
@@ -22,6 +22,80 @@ function topFunction() {
 }
 /* Fin del comportamiento del boton de Top */
 
+/* Funciones para cambiar imagenes con cada boton */
+
 function piscinaFunc() {
-	document.getElementById("niñaContainer").style.backgroundImage = "url('images/testimonios.jpg')";
+	document.getElementById("niñaContainer").style.backgroundImage = "url('images/PiscinaOlimpica.png')";
 }
+
+function piscinaKidsFunc() {
+	document.getElementById("niñaContainer").style.backgroundImage = "url('images/piscinaNinos.png')";
+}
+
+function parqueInFunc() {
+	document.getElementById("niñaContainer").style.backgroundImage = "url('images/parqueInfantil.png')";
+}
+
+function churrasqueraFunc() {
+	document.getElementById("areneroContainer").style.backgroundImage = "url('images/churrasqueraImg.png')";
+}
+
+function salonDeFiestaFunc() {
+	document.getElementById("areneroContainer").style.backgroundImage = "url('images/salonFiesta.png')";
+}
+
+function areneroFunc() {
+	document.getElementById("areneroContainer").style.backgroundImage = "url('images/areneroImg.png')";
+}
+
+function gimnasioFunc() {
+	document.getElementById("deportesContainer").style.backgroundImage = "url('images/gimnasioImg.png')";
+}
+
+function canchasFunc() {
+	document.getElementById("deportesContainer").style.backgroundImage = "url('images/canchasImg.png')";
+}
+
+function salaAeroFunc() {
+	document.getElementById("deportesContainer").style.backgroundImage = "url('images/aerobicosImg.png')";
+}
+
+/* Desde aca las funciones llaman a las imagenes para moviles */
+
+function piscinaDosFunc() {
+	document.getElementById("niñaContainer").style.backgroundImage = "url('images/PiscinaOlimpicaPeq.png')";
+}
+
+function piscinaKidsDosFunc() {
+	document.getElementById("niñaContainer").style.backgroundImage = "url('images/piscinaNinosPeq.png')";
+}
+
+function parqueInDosFunc() {
+	document.getElementById("niñaContainer").style.backgroundImage = "url('images/parqueInfantilPeq.png')";
+}
+
+function churrasqueraDosFunc() {
+	document.getElementById("areneroContainer").style.backgroundImage = "url('images/churrasqueraImgPeq.png')";
+}
+
+function salonDeFiestaDosFunc() {
+	document.getElementById("areneroContainer").style.backgroundImage = "url('images/salonFiestaPeq.png')";
+}
+
+function areneroDosFunc() {
+	document.getElementById("areneroContainer").style.backgroundImage = "url('images/areneroImgPeq.png')";
+}
+
+function gimnasioDosFunc() {
+	document.getElementById("deportesContainer").style.backgroundImage = "url('images/gimnasioImgPeq.png')";
+}
+
+function canchasDosFunc() {
+	document.getElementById("deportesContainer").style.backgroundImage = "url('images/canchasImgPeq.png')";
+}
+
+function salaAeroDosFunc() {
+	document.getElementById("deportesContainer").style.backgroundImage = "url('images/aerobicosImgPeq.png')";
+}
+
+/* Final funciones de botones cambiando imagenes */
